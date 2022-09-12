6dbe52838ac841de25d3556d21579e8dd94666b7
gigijeskai <luigindaco@gmail.com>
2022-09-12 16:48:55 +0200

    consegna

---
+++ js/naming.js
@@ -0,0 +1,30 @@
+var  pet_preferito = 'gatto';
+console.log( pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var Pet = 'cane';
+document.write(Pet);
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+document.write($pet);
+var _pet = 'leone';
+document.write(_pet );
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+var twelve = 12;
+console.log(twelve);
+
+document.getElementById("animale-preferito").innerHTML = "gatto";
+console.log();
+
+document.getElementById("animale-preferito").style.color = "red";
+
+
+
